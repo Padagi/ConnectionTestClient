4429b3e0714b6b10e74ab6c00ac2fa45f8237a95
Matt <Matt@MATT-PC>
2016-09-13 13:31:27 -0700

    Add project files.

---
+++ ConnectionTestClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
